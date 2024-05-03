b6e8762b7bdc3155c14a2802f5523631199b295e
brunogbitencourt <brunogbitencourt@hotmail.com>
2024-05-02 21:54:00 -0300

    Configurações Globais

--- include/GlobalConfig.h
+++ include/GlobalConfig.h
@@ -1,10 +1,12 @@
 //#define MODE 0 // Generates random values for the sensors and actuators
-#define MODE 1 // Read sensors
+#define MODE 0 // Read sensors
 
 
-// Actuators pins definition
+//--------------------------------------------------------------------------------//
+//------------------------------Actuators Variables-------------------------------//
+//--------------------------------------------------------------------------------//
 #define PUMP1_PIN  22
-#define PUMP2_PIN  26// Mudar
+#define PUMP2_PIN  26
 #define PUMP3_PIN  22
 
 #define MOTOR1_PIN 25
@@ -12,6 +14,22 @@
 
 #define VALVE1_PIN 23
 #define VALVE2_PIN 19
+//--------------------------------------------------------------------------------//
+//------------------------------Sensors Variables---------------------------------//
+//--------------------------------------------------------------------------------//
 
+//--------------------------------------------------------------------------------//
+//------------------------------Wifi Variables------------------------------------//
+//--------------------------------------------------------------------------------//
+#define WIFI_SSID "LIVE TIM_07A0_2G"
+#define WIFI_PASS "Brunolucas"
+// #define WIFI_SSID "iPhone B"
+// #define WIFI_PASS "bruno123"
 
-
+//--------------------------------------------------------------------------------//
+//------------------------------MQTT Variables------------------------------------//
+//--------------------------------------------------------------------------------//
+// Mosquitto
+#define MOSQUITTO_BROKER_HOST "test.mosquitto.org"
+#define MOSQUITTO_BROKER_PORT 1883
+#define MOSQUITTO_USERNAME "IoT_Mosquitto"
